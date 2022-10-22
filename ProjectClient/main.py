a4710f9c09037baa3f94a2e9d762b00f16c0ae6d
ViktorKozhuev <viktor.kozhuev@gmail.com>
2022-10-22 12:00:42 +0300

    second commit

---
+++ ProjectClient/main.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    ServerAdress = "https://localhost:5000/"
+    MessageId = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('messanger.ui', self)
+        # self.pushButton.clicked.connect(self.pushButton_clicked)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
